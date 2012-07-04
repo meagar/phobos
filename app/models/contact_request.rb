c8f8552b33a95d86f996c0c87c751020a2b8647f
Matthew Eagar <matthew.eagar@mosaic.com>
2012-07-03 23:23:33 -0400

    Fix error message for contact form email

--- app/models/contact_request.rb
+++ app/models/contact_request.rb
@@ -2,6 +2,6 @@ class ContactRequest < ActiveRecord::Base
   attr_accessible :body, :email, :name
 
   validates :name, :presence => true, :length => { :maximum => 255 }
-  validates :email, :format => { :with => /\A.*@.*\..*\z/, :message => "must contain a valid email address" }
+  validates :email, :format => { :with => /\A.*@.*\..*\z/ }
   validates :body, :presence => true
 end
